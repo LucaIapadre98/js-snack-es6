21ebde8420309e98a026b4abbf16dc900d751ec9
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-14 15:47:46 +0200

    snack 2

--- js/main.js
+++ js/main.js
@@ -30,3 +30,46 @@ const bikes = [
 
 let bike = bikes.find(bike => bike.peso == "10kg" );
 console.log(bike);
+
+
+// #Snack2
+//Creare un array di oggetti di squadre di calcio.
+//Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+//Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
+//Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti 
+// e stampiamo tutto in console.
+
+
+
+
+let points = Math.floor(Math.random() * 10);
+let fouls = Math.floor(Math.random() * 10);
+const players = [
+    {
+        name: "Andrea",
+        points: points,
+        fouls:  fouls,
+    },
+    {
+        nome: "Antonio",
+        points: points,
+        fouls:  fouls,
+       
+    },
+    {
+        nome: "Alessio",
+        points: points,
+        fouls:  fouls,
+    },
+    {
+        nome: "Angelo",
+        points: points,
+        fouls:  fouls,
+    },
+]
+console.log(points);
+console.log(fouls);
+console.log(players);
+
+
