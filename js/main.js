f5c8d54c033e0f41f3e013eb6b10350e1e37fe82
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-14 18:13:41 +0200

    correzioni

--- js/main.js
+++ js/main.js
@@ -1,38 +1,68 @@
-// #Snack 1
+// # Snack 1
 // Creare un array di oggetti:
 
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 
 // Stampare a schermo la bici con peso minore.
-
 const bikes = [
 {
-    "nome": "bicicetta1",
-    "peso":"10kg",
+    nome: "bicicetta1",
+    peso:"10kg",
 },
 {
-    "nome": "bicicetta2",
-    "peso":"15kg",
+    nome: "bicicetta2",
+    peso:"15kg",
 },
 {
-    "nome": "bicicetta3",
-    "peso":"20kg",
+    nome: "bicicetta3",
+    peso:"20kg",
 },
 {
-    "nome": "bicicetta4",
-    "peso":"25kg",
+    nome: "bicicetta4",
+    peso:"25kg",
 },
 {
-    "nome": "bicicetta5",
-    "peso":"30kg",
+    nome: "bicicetta5",
+    peso:"30kg",
 }
 ]
 
 let bike = bikes.find(bike => bike.peso == "10kg" );
-console.log(bike);
+//console.log(bike);
 
 
-// #Snack2
+
+
+const bikers =[
+    bicicletta1 = {
+        name: "one",
+        height:"15",
+    },
+    bicicletta2 = {
+        name: "two",
+        height:"10",
+    },
+    bicicletta3 = {
+        name: "three",
+        height:"25",
+    },
+    bicicletta4 = {
+        name: "four",
+        height:"30",
+    },
+]
+
+
+ 
+
+ 
+
+
+
+
+
+
+// # Snack2
 //Creare un array di oggetti di squadre di calcio.
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -41,57 +71,66 @@ console.log(bike);
 let points = Math.floor(Math.random() * 50);
 let fouls = Math.floor(Math.random() * 10);
 
+let points1 = Math.floor(Math.random() * 50);
+let fouls1 = Math.floor(Math.random() * 10);
+
+let points2 = Math.floor(Math.random() * 50);
+let fouls2 = Math.floor(Math.random() * 10);
+
+let points3 = Math.floor(Math.random() * 50);
+let fouls3 = Math.floor(Math.random() * 10);
+
 const players = [
     {
         name: "Andrea",
-        points: points,
-        fouls:  fouls,
+        points:"punti fatti" +"=" +  points,
+        fouls:"falli subiti" +":" +   fouls,
     },
     {
         name: "Antonio",
-        points: points,
-        fouls:  fouls,
+        points:"punti fatti" +"=" +  points1,
+        fouls:"falli subiti" +":" +   fouls1,
        
     },
     {
         name: "Alessio",
-        points: points,
-        fouls:  fouls,
+        points:"punti fatti" +"=" +  points2,
+        fouls:"falli subiti" +":" +   fouls2,
     },
     {
         name: "Angelo",
-        points: points,
-        fouls:  fouls,
+        points:"punti fatti" +"=" +  points3,
+        fouls:"falli subiti" +":" +   fouls3,
     },
 ]
 
-console.table(players);
+ console.table(players);
 
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti 
 // e stampiamo tutto in console.
  
-let fouls1 = Math.floor(Math.random() * 5);
+let fouls5 = Math.floor(Math.random() * 10);
 const player = [
     {
         name: "Andrea",
-        fouls1:  fouls,
+        fouls1: "falli subiti" +":" + fouls1,
     },
     {
         name: "Antonio",
-        fouls1:  fouls,
+        fouls1:"falli subiti" +":" +  fouls1,
        
     },
     {
         name: "Alessio",
-        fouls1:  fouls,
+        fouls1:"falli subiti" +":" +   fouls1,
     },
     {
         name: "Angelo",
-        fouls1:  fouls,
+        fouls1:"falli subiti" +":" +   fouls1,
     },
 ]
 
-console.table(player);
+//console.table(player);
 
 
 
